ae84fcc26dd38774a4d5c572535cf2ed63ffadbb
Valentin QIU <valentinqiu@gmail.com>
2022-08-15 21:45:47 +0200

    First Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Valentin Qiu on 15/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/SwiftUiTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiTutorialApp.swift
+//  Shared
+//
+//  Created by Valentin Qiu on 15/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
